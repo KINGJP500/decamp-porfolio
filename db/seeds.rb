5c0f445865ca78a7976a907b185d25befb9374d6
KINGJP500 <kingjpstar500@gmail.com>
2018-07-21 20:28:20 -0400

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+  Topic.create!(
+    title: "Topic#{topic}"
+
+  )
+end
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -20,7 +28,8 @@
      the release of Letraset sheets containing Lorem
      Ipsum passages, and more recently with desktop
      publishing software like Aldus PageMaker including
-     versions of Lorem Ipsum."
+     versions of Lorem Ipsum.",
+     topic_id: Topic.last.id
   )
 end
 
@@ -35,10 +44,32 @@ end
 
 puts "5 blog posts created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body: "Lorem Ipsum is simply dummy text
+    of the printing and typesetting industry.
+    Lorem Ipsum has been the industry's standard
+    dummy text ever since the 1500s, when an unknown
+    printer took a galley of type and scrambled it to
+    make a type specimen book. It has survived not
+    only five centuries, but also the leap into
+    electronic typesetting, remaining essentially
+    unchanged. It was popularised in the 1960s with
+    the release of Letraset sheets containing Lorem
+    Ipsum passages, and more recently with desktop
+    publishing software like Aldus PageMaker including
+    versions of Lorem Ipsum.",
+    main_image: "https://placeimg.com/640/480/any?t=1530660377192",
+    thumb_image: "https://placeimg.com/640/480/any?t=1530660377192"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Lorem Ipsum is simply dummy text
     of the printing and typesetting industry.
     Lorem Ipsum has been the industry's standard
