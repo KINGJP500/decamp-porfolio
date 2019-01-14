c8f77690686731717a68aa8f308e3a63d15fc257
KINGJP500 <kingjpstar500@gmail.com>
2019-01-13 19:59:00 -0500

    Refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,8 +11,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-        technologies_attributes: [:name]))
+      @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -32,7 +31,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'This record was successfully updated.' }
         #format.json { render :show, status: :ok, location: @blog }
       else
@@ -59,4 +58,8 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  private
+  def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+  end
 end
